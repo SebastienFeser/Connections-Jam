871153f41d0fa6c8007adf2484f187c3e547528d
SebastienFeser <sedukafe@gmail.com>
2024-09-20 21:26:37 +0200

    added goods production

--- Assets/Scripts/ProductionPoint.cs
+++ Assets/Scripts/ProductionPoint.cs
@@ -6,6 +6,21 @@ public class ProductionPoint : MonoBehaviour
 {
     int productCount;
     List<DistributionPoint> connections;
+    [SerializeField] float productTimer;
+    float actualTime = 0;
+    [SerializeField] int maximumGoods;
 
+    private void Update()
+    {
+        actualTime += Time.deltaTime;
+        if(actualTime > productTimer)
+        {
+            if (productCount < maximumGoods)
+            {
+                productCount++;
+            }
+            actualTime -= productTimer;
+        }
+    }
 
 }
